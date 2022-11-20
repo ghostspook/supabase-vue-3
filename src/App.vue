794403eb3f3ce8dbab26446ed8459e5d23ddf22d
Rafael Castillo <pcastill@mac.com>
2022-11-20 14:51:13 -0500

    Adding OTP email auth and a couple of pages

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,25 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+  import { onMounted, ref } from 'vue'
+  import Account from './components/Account.vue'
+  import Auth from './components/Auth.vue'
+  import { supabase } from './supabase'
+
+  const session = ref()
+
+  onMounted(() => {
+    supabase.auth.getSession().then(({ data }) => {
+      session.value = data.session
+    })
+
+    supabase.auth.onAuthStateChange((_, _session) => {
+      session.value = _session
+    })
+  })
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container" style="padding: 50px 0 100px 0">
+    <Account v-if="session" :session="session" />
+    <Auth v-else />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
